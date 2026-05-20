85f3507b8ff02959872c52436f97649a48458152
aryanhgit <aryan01eio@gmail.com>
2026-05-21 00:18:39 +0530

    add appstate

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import sys
 from PySide6.QtWidgets import QApplication
 from ui.main_window import MainWindow
+from utils.state import AppState
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     app.setApplicationName("GitView")
 
-    window = MainWindow()
+    state = AppState()
+    window = MainWindow(state)
     window.show()
     sys.exit(app.exec())
