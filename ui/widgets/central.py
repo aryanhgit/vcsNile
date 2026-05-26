4ee238d69d56d8f9d87d72b92e1b64fa4821d422
aryanhgit <aryan01eio@gmail.com>
2026-05-26 22:33:58 +0530

    add reflog button

--- ui/widgets/central.py
+++ ui/widgets/central.py
@@ -4,6 +4,7 @@ from PySide6.QtWidgets import QTabWidget
 from ui.widgets.dag import DagCanvas
 from ui.widgets.explorer import ObjectExplorerTab
 from ui.widgets.staging import StagingWidget
+from ui.widgets.reflog import ReflogTab
 
 class CentralArea(QTabWidget):
     def __init__(self, state: AppState):
@@ -13,3 +14,4 @@ class CentralArea(QTabWidget):
         self.addTab(DagCanvas(state), "Commit Graph")
         self.addTab(StagingWidget(state), "Staging")
         self.addTab(ObjectExplorerTab(state), "Objects")
+        self.addTab(ReflogTab(state), "Reflog")
